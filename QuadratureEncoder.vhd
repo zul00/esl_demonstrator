ce3e38eb886564a7598ac4be14aaa06e09cf9cd7
zul00 <zul.zulkarnaen@gmail.com>
2017-05-08 10:47:30 +0200

    Update encoder from Tom

--- QuadratureEncoder.vhd
+++ QuadratureEncoder.vhd
@@ -1,81 +1,85 @@
 LIBRARY ieee;
-USE ieee.std_logic_1164.all;
+USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 ENTITY QuadratureEncoder IS
-  PORT (
-         a  : IN std_logic;    -- Encoder signal
-         b  : IN std_logic;    -- Encoder signal
-         clk : IN std_logic;
-         reset : IN std_logic;
-         ctr : OUT std_logic_vector(11 DOWNTO 0)  -- Encoder ctr 12 bit
-       );
-END ENTITY;
+  GENERIC (w : positive := 12);
+  PORT (clk : IN std_logic;
+        reset : IN std_logic;
+        a  : IN  std_logic;
+        b : IN std_logic;
+        ctr : OUT std_logic_vector(w-1 DOWNTO 0));
+END QuadratureEncoder;
 
-ARCHITECTURE bhv OF QuadratureEncoder IS
-  SIGNAL a_r : std_logic_vector(1 DOWNTO 0);
-  SIGNAL b_r : std_logic_vector(1 DOWNTO 0);
-  SIGNAL dir : std_logic_vector(1 DOWNTO 0);
+ARCHITECTURE behaviour OF QuadratureEncoder IS
+
 BEGIN
-  event:PROCESS(clk, reset)
-  VARIABLE ctr_tmp : signed(ctr'RANGE);
+
+  PROCESS(clk, reset)
+    TYPE states IS (S01, S10, S11, S00);
+    VARIABLE state : states;
+    VARIABLE lcl_counter : signed(ctr'RANGE);
   BEGIN
-    IF reset='1' THEN
-      ctr <= (OTHERS => '0');
-      ctr_tmp := to_signed(0, ctr'LENGTH);
-      a_r <= (OTHERS => '0');
-      b_r <= (OTHERS => '0');
+    IF reset = '1' THEN
+      lcl_counter := to_signed(0, ctr'LENGTH);
+
+      IF a='1' AND b='1' THEN 
+        state := S11;
+      ELSIF a='0' AND b='0' THEN
+        state := S00;
+      ELSIF a='0' AND b='1' THEN
+        state := S01;
+      ELSIF a='1' AND b='0' THEN
+        state := S10;
+      END IF;
+
     ELSIF rising_edge(clk) THEN
-      a_r <= a_r(0) & a;
-      b_r <= b_r(0) & b;
-
-      -- Check direction
-      CASE a_r IS
-        WHEN "01" =>    -- rising edge
-          IF b = '1' THEN
-            dir <= "01";   -- up
-          ELSIF b = '0' THEN
-            dir <= "10";   -- down
+      CASE state IS 
+        WHEN S01 => 
+          IF a='1' AND b='1' THEN 
+            lcl_counter := lcl_counter-1;
+            state := S11;
+          ELSIF a='0' AND b='0' THEN
+            lcl_counter := lcl_counter+1;
+            state := S00;
           END IF;
-        WHEN "10" =>    -- falling edge
-          IF b = '1' THEN
-            dir <= "10";   -- down
-          ELSIF b = '0' THEN
-            dir <= "01";   -- up
+        WHEN S10 => 
+          IF a='1' AND b='1' THEN 
+            lcl_counter := lcl_counter+1;
+            state := S11;
+          ELSIF a='0' AND b='0' THEN
+            lcl_counter := lcl_counter-1;
+            state := S00;
           END IF;
-        WHEN OTHERS =>  -- no event
-      END CASE;
-
-      CASE b_r IS
-        WHEN "01" =>    -- rising edge
-          IF a = '1' THEN
-            dir <= "10";   -- down
-          ELSIF a = '0' THEN
-            dir <= "01";   -- up
+        WHEN S11 => 
+          IF a='1' AND b='0' THEN 
+            lcl_counter := lcl_counter-1;
+            state := S10;
+          ELSIF a='0' AND b='1' THEN
+            lcl_counter := lcl_counter+1;
+            state := S01;
           END IF;
-        WHEN "10" =>    -- falling edge
-          IF a = '1' THEN
-            dir <= "01";   -- up
-          ELSIF a = '0' THEN
-            dir <= "10";   -- down
+        WHEN S00 => 
+          IF a='1' AND b='0' THEN 
+            lcl_counter := lcl_counter+1;
+            state := S10;
+          ELSIF a='0' AND b='1' THEN
+            lcl_counter := lcl_counter-1;
+            state := S01;
           END IF;
-        WHEN OTHERS =>  -- last check for no event
-          IF ((a_r(0) XOR a_r(1)) = '0') THEN
-            dir <= "00";
-          END IF;
-      END CASE;
-
-      -- Update counter
-      CASE dir IS
-        WHEN "01" =>  -- up
-          ctr_tmp := ctr_tmp + 1;
-        WHEN "10" =>  -- down
-          ctr_tmp := ctr_tmp - 1;
         WHEN OTHERS =>
-          ctr_tmp := ctr_tmp;
+          IF a='1' AND b='1' THEN 
+            state := S11;
+          ELSIF a='0' AND b='0' THEN
+            state := S00;
+          ELSIF a='0' AND b='1' THEN
+            state := S01;
+          ELSIF a='1' AND b='0' THEN
+            state := S10;
+          END IF;
       END CASE;
-
-      ctr <= std_logic_vector(ctr_tmp);
     END IF;
+
+    ctr <= std_logic_vector(lcl_counter);
   END PROCESS;
-END ARCHITECTURE bhv;
+END behaviour;
